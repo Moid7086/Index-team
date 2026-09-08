ad689df5595e75c1e8794b14de25bbcc1d11ce03
Moid7086 <leonardomoisessr@gmail.com>
2026-09-08 08:07:18 -0600

    Delete Indexteam directory

--- Indexteam/moipracticas/js.js
+++
@@ -1,9 +0,0 @@
-const buttons = document.querySelectorAll('.btnref');
-
-buttons.forEach(button=>{
-    button.addEventListener('click', function(){
-        const value = this.value;
-        window.location.href = this.value
-        console.log(""+ this.value)
-    })
-})
